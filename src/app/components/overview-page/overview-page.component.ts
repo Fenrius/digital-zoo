acb2461de1538b373e1595319c1bf0a63513137b
Kristof Jahnke <jahnke.kristof2@gmail.com>
2024-08-08 21:21:11 +0200

    feat: add placeholder data for table

--- src/app/components/overview-page/overview-page.component.ts
+++ src/app/components/overview-page/overview-page.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {TableComponent} from "../table/table.component";
+import {AnimalDataInterface} from "../../utils/animal-data.interface";
+import {ButtonComponent} from "../button/button.component";
 
 @Component({
   selector: 'app-overview-page',
   standalone: true,
-  imports: [],
+  imports: [
+    TableComponent,
+    ButtonComponent
+  ],
   templateUrl: './overview-page.component.html',
   styleUrl: './overview-page.component.css'
 })
 export class OverviewPageComponent {
-
+  public animalData = signal<AnimalDataInterface[]>(
+    [
+    {name: 'Moonpie', weight: 2, power: 'Djukkedi pa', extinct: '2020'},
+    {name: 'Dodo', weight: 4.5, power: 'sitzen', extinct: 'langer Zeit'}
+  ]);
 }

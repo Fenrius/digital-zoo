2e54352089c46887343cea2c72d2144ff435a505
Kristof Jahnke <jahnke.kristof2@gmail.com>
2024-08-08 21:10:16 +0200

    feat: add pages and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'animal-overview',
+  },
+  {
+    path: 'animal-overview',
+    loadComponent: () =>
+      import('./components/overview-page/overview-page.component').then(
+        m => m.OverviewPageComponent
+      )
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/not-found-page/not-found-page.component').then(
+        m => m.NotFoundPageComponent
+      )
+  }
+];
